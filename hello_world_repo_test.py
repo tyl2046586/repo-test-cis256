7443c6fffbe51e2b9c08ed58ce7aa375de6aad9d
tyl2046586 <tyl2046586@maricopa.edu>
2024-11-03 00:09:24 -0700

    Added extra text to the Hello World string.

--- hello_world_repo_test.py
+++ hello_world_repo_test.py
@@ -3,7 +3,9 @@ Tyler Hallick
 Module 08 Lab Assignment
 Part B
 
-This program is a test for creating a repository.
+This program is a test for creating a repository. It has been modified
+after it's initial commit and its changes pushed to the repository.
+URL : https://github.com/tyl2046586/repo-test-cis256
 """
 
-print("Hello world.")
+print("Hello world. Nice weather we're having!")
